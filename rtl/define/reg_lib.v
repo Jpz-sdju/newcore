8a21f9b16d8d8504b252a2a7aac5005114e39695
jpz-sdju <rengeyouzh1@163.com>
2022-12-03 23:40:09 +0800

    update reg lib

--- rtl/define/reg_lib.v
+++ rtl/define/reg_lib.v
@@ -2,7 +2,17 @@ module dff #(
     parameter WIDTH=1
 ) (
     input [WIDTH-1:0] din,
+    input clk,
+    input rst_l,
     
+    output [WIDTH-1:0] dout
 );
-    
+    always @(posedge clk or negedge rst_l) begin
+        if (~rst_l) begin
+            dout <= WIDTH'b0;
+        end
+        else begin
+            dout <= din;
+        end
+    end
 endmodule
